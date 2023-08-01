b7db49cea494bbf695cf216dec3cb6b9274306e1
HamidGarayev3 <hamidgarayev3@gmail.com>
2023-08-01 16:55:46 +0400

    MalMedaxil Page Almost finished

--- AwesomeProject/src/screens/MalMedaxil.tsx
+++ AwesomeProject/src/screens/MalMedaxil.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useCallback } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
-import { SvgUri } from 'react-native-svg';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, StyleSheet, Text, TouchableOpacity, Image, TextInput, Modal, Button } from 'react-native';
 import Animated, {
     useAnimatedGestureHandler,
     useAnimatedStyle,
@@ -8,122 +7,137 @@ import Animated, {
     withSpring,
     interpolate,
     Extrapolate,
-    runOnJS
 } from 'react-native-reanimated';
-import { PanGestureHandler, PanGestureHandlerGestureEvent, ScrollView } from 'react-native-gesture-handler';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
-type CardProps = {
+type Card = {
     id: number;
-    onDelete: (id: number) => void;
+    translateX: Animated.SharedValue<number>;
 };
 
-const MalMedaxil: React.FC<CardProps> = ({ id, onDelete }) => {
-    const translateX = useSharedValue(0);
-    const isDeleteVisible = useSharedValue(0);
-
-    const gestureHandler = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
-        onActive: (event) => {
-            translateX.value = event.translationX;
-        },
-        onEnd: () => {
-            if (translateX.value < -54) {
-                translateX.value = withSpring(-54, { damping: 10, stiffness: 100, mass: 1, overshootClamping: true });
-                isDeleteVisible.value = 1;
-            } else {
-                translateX.value = withSpring(0, { damping: 10, stiffness: 100, mass: 1, overshootClamping: false });
-                isDeleteVisible.value = 0;
-            }
-        },
-    });
-
-    const animatedStyle = useAnimatedStyle(() => {
-        return {
-            transform: [{ translateX: translateX.value }],
-        };
-    });
-
-    const deleteStyle = useAnimatedStyle(() => {
-        return {
-            opacity: interpolate(
-                translateX.value,
-                [-54, 0],
-                [1, 0],
-                Extrapolate.CLAMP
-            ),
-        };
-    });
-
-    const handleDelete = () => {
-        runOnJS(onDelete)(id);
-    };
-
-    const handleCardPress = () => {
-        // When the card is pressed, animate the translation to reveal the delete button
-        translateX.value = withSpring(-54, { damping: 10, stiffness: 100, mass: 1, overshootClamping: true });
-        isDeleteVisible.value = 1;
-    };
-
-    return (
-        <ScrollView>
-            <View style={styles.container}>
-            <Animated.View style={[styles.deleteContainer, deleteStyle]}>
-                <TouchableOpacity onPress={handleDelete}>
-                    <Image source={require('../assets&styles/trash.png')} style={{ width: 24, height: 24 }} />
-                </TouchableOpacity>
-            </Animated.View>
-            <PanGestureHandler onGestureEvent={gestureHandler}>
-                <Animated.View style={[styles.card, animatedStyle]}>
-                    <TouchableOpacity activeOpacity={1} style={{ flex: 1 }} onPress={handleCardPress}>
-                        <View style={styles.topHalf}>
-                            <Text>Top Half</Text>
-                        </View>
-                        <View style={styles.bottomHalf}>
-                            <View style={styles.bottomLeft}><Text>Bottom Left</Text></View>
-                            <View style={styles.bottomRight}><Text>Bottom Right</Text></View>
-                        </View>
-                    </TouchableOpacity>
-                </Animated.View>
-            </PanGestureHandler>
-        </View>
-        </ScrollView>
-    );
-}
-
-const App = () => {
-    const [cards, setCards] = useState([
-        { id: 1 },
-        { id: 2 },
-        { id: 3 },
+const App: React.FC = () => {
+    const [cards, setCards] = useState<Card[]>([
+        { id: 1, translateX: useSharedValue(0) }
     ]);
 
-    const handleDelete = useCallback((id) => {
-        setCards(cards.filter((card) => card.id !== id));
-    }, [cards]);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const addCard = () => {
+        setCards(prevCards => [
+            ...prevCards, 
+            { id: prevCards.length + 1, translateX: useSharedValue(0) }
+        ]);
+        setModalVisible(false);
+    };
 
     return (
         <View style={styles.appContainer}>
-            {cards.map((card) => (
-                <MalMedaxil key={card.id} id={card.id} onDelete={handleDelete} />
-            ))}
+            <View style={styles.searchContainer}>
+                <Image source={require('../assets&styles/back.png')} style={{ width: 24, height: 24 }} />
+                <TextInput style={styles.searchInput} placeholder="Search..." placeholderTextColor="#F4F9FD"/>
+                <Image source={require('../assets&styles/filter.png')} style={{ width: 24, height: 24 }} />
+            </View>
+            <ScrollView style={styles.container}>
+                {cards.map((card) => {
+                    const gestureHandler = useAnimatedGestureHandler({
+                        onStart: (_, ctx) => {
+                            ctx.startX = card.translateX.value;
+                        },
+                        onActive: (event, ctx) => {
+                            card.translateX.value = ctx.startX + event.translationX;
+                        },
+                        onEnd: () => {
+                            if (card.translateX.value < -54) {
+                                card.translateX.value = withSpring(-54);
+                            } else {
+                                card.translateX.value = withSpring(0);
+                            }
+                        },
+                    });
+
+                    const animatedStyle = useAnimatedStyle(() => {
+                        return {
+                            transform: [{ translateX: card.translateX.value }],
+                        };
+                    });
+
+                    const deleteStyle = useAnimatedStyle(() => {
+                        return {
+                            opacity: interpolate(
+                                card.translateX.value,
+                                [-54, 0],
+                                [1, 0],
+                                Extrapolate.CLAMP
+                            ),
+                        };
+                    });
+
+                    return (
+                        <View key={card.id} style={{ marginTop: 20 }}>
+                            <Animated.View style={[styles.deleteContainer, deleteStyle]}>
+                                <TouchableOpacity>
+                                    <Image source={require('../assets&styles/trash.png')} style={{ width: 24, height: 24 }} />
+                                </TouchableOpacity>
+                            </Animated.View>
+                            <PanGestureHandler onGestureEvent={gestureHandler}>
+                                <Animated.View style={[styles.card, animatedStyle]}>
+                                    <TouchableOpacity activeOpacity={1} style={{ flex: 1 }}>
+                                        <View style={styles.topHalf}>
+                                            <Text style={styles.topHalfText1}>Top Half</Text>
+                                            <Text style={styles.topHalfText2}>Another Text</Text>
+                                        </View>
+                                        <View style={styles.bottomHalf}>
+                                            <View style={styles.bottomLeft}><Text style={styles.bottomLeftText}>Bottom Left</Text></View>
+                                            <View style={styles.bottomRight}><Text style={styles.bottomRightText}>Bottom Right</Text></View>
+                                        </View>
+                                    </TouchableOpacity>
+                                </Animated.View>
+                            </PanGestureHandler>
+                        </View>
+                    );
+                })}
+            </ScrollView>
+            <TouchableOpacity style={styles.floatingButton} onPress={() => setModalVisible(true)}>
+                <Text style={{fontSize:30,fontWeight:'bold'}}>+</Text>
+            </TouchableOpacity>
+            <Modal animationType="slide" transparent={true} visible={modalVisible}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <TextInput style={styles.modalInput} placeholder="Input 1" />
+                        <TextInput style={styles.modalInput} placeholder="Input 2" />
+                        <Button onPress={addCard} title="OK" />
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
-}
+};
+
+
 
 const styles = StyleSheet.create({
     appContainer: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor:'#1F1D2B',
+    },
+    searchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        backgroundColor:'#1F1D2B'
+        paddingHorizontal: 20,
+        marginTop: 20
+    },
+    searchInput: {
+        flex: 1,
+        marginLeft: 10,
+        marginRight: 10,
+        color: '#F4F9FD',
+        borderBottomColor: '#F4F9FD',
+        borderBottomWidth: 1
     },
     container: {
-        width: 321,
-        height: 96,
-        overflow: 'hidden',
-        marginBottom: 10,
-        borderWidth:1,
-        borderColor:'white',
-        borderRadius:8
+        flex: 1,
+        paddingHorizontal:20,
     },
     deleteContainer: {
         position: 'absolute',
@@ -137,27 +151,98 @@ const styles = StyleSheet.create({
     },
     card: {
         width: '100%',
-        height: '100%',
+        height: 130,
+        overflow: 'hidden',
+        borderWidth:1,
+        borderColor:'white',
+        borderRadius:8,
         flexDirection: 'column',
-
     },
     topHalf: {
-        flex: 1,
+        flex: 0.6,
         borderColor:'white',
-        borderBottomWidth:1
+        borderBottomWidth:1,
+        padding: 10
     },
     bottomHalf: {
-        flex: 1,
+        flex: 0.4,
         flexDirection: 'row',
     },
     bottomLeft: {
         flex: 1,
         borderColor:'white',
-        borderRightWidth:1
+        borderRightWidth:1,
+        padding: 10
     },
     bottomRight: {
+        flex: 0.5,
+        borderColor:'white',
+        padding: 10
+    },
+    topHalfText1: {
+        fontSize:12,
+        color:'#F4F9FD',
+        marginTop:10
+    },
+    topHalfText2: {
+        fontSize:18,
+        color:'#F4F9FD',
+        fontWeight:"700",
+    },
+    bottomLeftText: {
+        fontSize:16,
+        color:'#F4F9FD'
+    },
+    bottomRightText: {
+        fontSize:16,
+        color:'#F4F9FD'
+    },
+    floatingButton: {
+        backgroundColor: 'green',
+        position: 'absolute',
+        right: 30,
+        bottom: 30,
+        borderRadius: 50/2,
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    centeredView: {
         flex: 1,
-        borderColor:'white'
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    modalInput: {
+        height: 40,
+        width: 200,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
     },
 });
 
